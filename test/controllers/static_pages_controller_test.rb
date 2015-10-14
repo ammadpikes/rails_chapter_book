fdaeb1f173583f75efc987be440e6d9550de694f
Ammad Ahmed <ammad.ahmed@pikessoft.com>
2015-10-14 16:48:38 +0500

    Test File Enhanced

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,25 +1,28 @@
 require 'test_helper'
 
-
 class StaticPagesControllerTest < ActionController::TestCase
   
+  def setup
+     @title_partial = "Ruby On Rails"
+   end 
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby On Rails"
+    assert_select "title", "Home | #{@title_partial}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby On Rails"
+    assert_select "title", "Help | #{@title_partial}"
   end
 
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby On Rails"
+    assert_select "title", "About | #{@title_partial}"
   end
 
 
