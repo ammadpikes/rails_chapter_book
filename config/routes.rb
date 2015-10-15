8241b49fede1fd97685bb6703642a062c36be484
Ammad Ahmed <ammad.ahmed@pikessoft.com>
2015-10-15 15:36:01 +0500

    User controller & Model Created (Final)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do  
+  get 'user/new'
+
   root 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/contact'
   get 'static_pages/help'
-  get 'user/new'
 end
