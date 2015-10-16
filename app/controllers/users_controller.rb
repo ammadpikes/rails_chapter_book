fd7005baead476be2a2a532fd4dcd9622168688f
Ammad Ahmed <ammad.ahmed@pikessoft.com>
2015-10-16 09:58:20 +0500

    Sign Up Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = @user.save(params[:user])
-    if user.save
-
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Ruby on Rails!"
+      redirect_to @user
     else
       render 'new'
     end
   end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 
 end
